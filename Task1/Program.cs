3ea390cc892890280de69feb41f89fab6097be53
Tasa <greatwillow@mail.ru>
2022-07-26 21:23:48 +0300

    first task

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintNumber (int m, int n)
+{
+    
+    if (m<n)
+        {Console.Write($"{m} ");PrintNumber(m+1, n);}
+    else {Console.Write(n);
+   }
+}
+Console.WriteLine("введите число m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число n");
+int n = int.Parse(Console.ReadLine());
+
+PrintNumber(m,n);
