796d49d7e14f78a9b7ed594bf4b69b95d5ae941a
Tasa <greatwillow@mail.ru>
2022-07-26 21:33:18 +0300

    second task

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿void PrintNumber (int m, int n, int s)
+{
+    if (m<=n)
+        {
+            s=s+m;
+            PrintNumber(m+1, n, s);
+        }
+    else 
+    {
+        Console.Write(s);
+    }
+}
+Console.WriteLine("введите число m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число n");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+PrintNumber(m,n, sum);
