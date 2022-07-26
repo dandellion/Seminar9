8c9cb3c7f4e5d55ade712f9ca1d5b0905e1fd9b2
Tasa <greatwillow@mail.ru>
2022-07-26 21:59:26 +0300

    third task

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿int Akkerman (int m, int n)
+{
+    if (m==0)
+        {
+            return n+1;
+        }
+    else if(m>0&&n==0)
+        {
+            return Akkerman(m-1,1);
+        }
+    else
+    {
+       return Akkerman (m-1, Akkerman(m,n-1));
+    }
+    
+}
+Console.WriteLine("введите число m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число n");
+int n = int.Parse(Console.ReadLine());
+int result = Akkerman(m,n);
+Console.WriteLine(result);
